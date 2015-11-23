b0a85107c989d09c1bce39f28ef4d910e3997966
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2015-11-23 21:06:25 +0100

    Appliance

--- Smart Grid Simulation/src/model/Appliance.java
+++ Smart Grid Simulation/src/model/Appliance.java
@@ -1,14 +1,54 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
-/**
- *
- * @author Martin
- */
-public class Appliance {
+import java.util.ArrayList;
+
+public class Appliance 
+{
+    private ArrayList<Double> energyCost;
+    private int obligatoryAfter;
+    private int framesDone;
     
+    public boolean commited;
+    
+    public Appliance(ArrayList<Double> energyCost, int obligatoryAfter) 
+    {
+        this.energyCost = energyCost;
+        this.obligatoryAfter = obligatoryAfter;
+        this.framesDone = 0;
+        this.commited = false;
+    }
+    
+    public boolean hasEnded()
+    {
+        return framesDone >= energyCost.size();
+    }
+   
+    public double getNextEnergyCost()
+    {
+        return energyCost.get(framesDone);
+    }
+    
+    public double getPressure( int framesLeft )
+    {
+        return commited ? 1.0/ obligatoryAfter : 0;
+    }
+    
+    public void commit()
+    {
+        framesDone = 1;
+        commited = true;
+    }
+    
+    public void passFrame()
+    {
+        if( commited )
+        {
+            framesDone++;
+        }
+        else
+        {
+            obligatoryAfter--;
+        }
+    }
 }
