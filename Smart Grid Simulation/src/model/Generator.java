f6e5a238635d12c1f58e3dcd12b630de733b90fe
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2015-11-23 21:33:53 +0100

    Simple Generator and SolarPanels

--- Smart Grid Simulation/src/model/Generator.java
+++ Smart Grid Simulation/src/model/Generator.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
 
-/**
- *
- * @author Martin
- */
-public class Generator {
-    
+public abstract class Generator 
+{
+    public abstract double getGenerated( Weather weather );    
 }
