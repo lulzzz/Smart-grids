4249ed46423aa3835f49759e459fc384a472505c
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2015-12-29 03:58:52 +0100

    Visualization: Nearest implementation

---
+++ Visualization/RadPro Visualization/Assets/Scripts/Nearest.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class Nearest : MonoBehaviour
+{
+    public void Start()
+    {
+        List<Vector3> positions = new List<Vector3>();
+        foreach (Transform child in gameObject.transform)
+        {
+            positions.Add(child.position);
+        }
+        File.WriteAllLines("tree.txt", makeWires(positions));
+    }
+
+    public string[] makeWires( List<Vector3> positions )
+    {
+        List<string> res = new List<string>();
+        res.Add("Edges:");
+        List<int> done = new List<int>();
+        done.Add(0);
+
+        for (int i = 1; i < positions.Count; i++)
+        {
+            // Get vertex at minimal distance
+            double shortest = Vector3.Distance(positions[i], positions[i-1]);
+            int index = 0;
+
+            for (int other = 0; other < positions.Count; other++)
+            {
+                if (other == i) continue;
+                double distance = Vector3.Distance(positions[i], positions[other]);
+                if (distance < shortest)
+                {
+                    shortest = distance;
+                    index = other;
+                }
+            }
+
+            // Add wire from i to other
+            res.Add(string.Format("{0}\t{1}\n", i, index));
+        }
+        return res.ToArray(); 
+    }
+}
