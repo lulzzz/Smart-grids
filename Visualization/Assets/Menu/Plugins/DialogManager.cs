4c31b06d5a2185f7b2fb68c346d2ab98c5cd2e36
Martin Azpillaga Aldalur <martin.azpillaga@gmail.com>
2016-01-08 22:25:24 +0100

    open dialog in build

---
+++ Visualization/Assets/Menu/Plugins/DialogManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+public class DialogManager : MonoBehaviour
+{
+    private static OpenFileDialog openDialog;
+
+    [DllImport("user32.dll")]
+    private static extern void OpenFileDialog();
+
+    public static string showDialog()
+    {
+        openDialog = new OpenFileDialog();
+        openDialog.InitialDirectory = UnityEngine.Application.dataPath;
+        openDialog.Filter = "obj files|*.OBJ";
+        openDialog.Title = "Select the city model.";
+        openDialog.Multiselect = false;
+        string[] result = null;
+        if (openDialog.ShowDialog() == DialogResult.OK)
+        {
+            result = openDialog.FileNames;
+        }
+        if (openDialog.FileName == string.Empty)
+        {
+            result = null;
+        }
+        openDialog = null;
+        return result[0];
+    }
+}
--- Visualization/Assets/Menu/Scripts/loadModelFromFile.cs
+++ Visualization/Assets/Menu/Scripts/loadModelFromFile.cs
@@ -1,45 +1,16 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
 
 public class loadModelFromFile : MonoBehaviour
 {
     [SerializeField]
-    private string path;
-    [SerializeField]
-    private float platformRadius;
-    private OpenFileDialog openDialog;
- 
-    [DllImport("user32.dll")]
-    private static extern void OpenFileDialog(); //in your case : OpenFileDialog
+    private GameObject platform;
 
-    public void Start()
+    public void OnMouseDown()
     {
-        string path = open();
+        string path = DialogManager.showDialog();
         loadMesh(path);
     }
-
-    public string open()
-    {
-        openDialog = new OpenFileDialog();
-        openDialog.InitialDirectory = UnityEngine.Application.dataPath;
-        openDialog.Filter = "obj files|*.OBJ";
-        openDialog.Title = "Select some music you want to listen to during this Game session.";
-        openDialog.Multiselect = true;
-        string[] result = null;
-        if (openDialog.ShowDialog() == DialogResult.OK)
-        {
-            result = openDialog.FileNames;
-        }
-        if (openDialog.FileName == string.Empty)
-        {
-            result = null;
-        }
-        openDialog = null;
-        return result[0];
-    }
-
+    
     public void loadMesh( string path )
     {
         Mesh mesh = ObjImporter.ImportFile(path);
@@ -47,12 +18,7 @@ public class loadModelFromFile : MonoBehaviour
         GetComponent<MeshFilter>().mesh = mesh;
         float size = Mathf.Max(mesh.bounds.size.x, mesh.bounds.size.z);
         // Some math
-        float ratio = .9f * platformRadius / (Mathf.Sqrt(2) * size);
-        gameObject.transform.localScale = new Vector3
-            (
-                transform.localScale.x * ratio,
-                transform.localScale.y,
-                transform.localScale.z * ratio
-            );
+        float ratio = .9f * platform.transform.localScale.x / (Mathf.Sqrt(2) * size);
+        gameObject.transform.localScale = ratio * transform.localScale;
     }
 }
